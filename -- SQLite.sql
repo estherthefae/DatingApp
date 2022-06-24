4bc8d9bc78062b0c412734d7d907198be6861022
unknown <esther.anakor@stu.cu.edu.ng>
2022-06-24 16:26:26 +0100

    initial commit

---
+++ -- SQLite.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Esther");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2, "James");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Janet");
